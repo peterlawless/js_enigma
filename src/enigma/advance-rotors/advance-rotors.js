5a05b816397f0fd9cb8666c5195adc16f813a1e2
Peter Lawless <peterlawless223@gmail.com>
2020-08-19 22:32:42 -0400

    cleaner implementation of advanceRotors

--- src/enigma/advance-rotors/advance-rotors.js
+++ src/enigma/advance-rotors/advance-rotors.js
@@ -1,30 +1,22 @@
 import { alphabetLoopIncrement } from "../../utils";
 
-const advanceRotors = settings => {
-  let rotorSetting, rotor, rotorPosition, isOnTurnoverLetter;
-  let newSettings = [];
-  // rightmost rotor always advances because its ratchet is completely exposed
-  let shouldRotorAdvance = true;
-  for (let i = settings.length - 1; i >= 0; i--) {
-    rotorSetting = settings[i];
-    ({ rotor, rotorPosition } = rotorSetting);
-    // https://www.youtube.com/watch?v=5StZlF-clPc
-    // Enigma's "double step anomaly" turns over a rotor if:
-    // 1) the rotor is NOT the leftmost AND it is on a turnover letter
-    // 2) its right neighbor is on a turnover letter
-    isOnTurnoverLetter = Boolean(
-      rotor.turnoverLetters[rotorPosition] && i !== 0
-    );
-    newSettings.unshift({
-      ...rotorSetting,
-      rotorPosition:
-        shouldRotorAdvance || isOnTurnoverLetter
-          ? alphabetLoopIncrement(rotorPosition)
-          : rotorPosition
-    });
-    shouldRotorAdvance = isOnTurnoverLetter;
-  }
-  return newSettings;
-};
+const isOnTurnoverLetter = ({ rotor, rotorPosition }) =>
+  Boolean(rotor.turnoverLetters[rotorPosition]);
+
+// https://www.youtube.com/watch?v=5StZlF-clPc
+// Enigma's "double step anomaly" turns over a rotor if:
+// 1) the rotor is NOT the leftmost AND it is on a turnover letter
+// 2) its right neighbor is on a turnover letter
+const advanceRotors = settings =>
+  settings.map((rotorSetting, index, currentSettings) => {
+    const { rotorPosition } = rotorSetting;
+    const isSlowRotor = index === 0;
+    const isFastRotor = index === currentSettings.length - 1;
+    return isFastRotor || // fast rotor always advances because its ratchet is completely exposed
+      (!isSlowRotor && isOnTurnoverLetter(rotorSetting)) ||
+      isOnTurnoverLetter(currentSettings[index + 1])
+      ? { ...rotorSetting, rotorPosition: alphabetLoopIncrement(rotorPosition) }
+      : rotorSetting;
+  });
 
 export default advanceRotors;
