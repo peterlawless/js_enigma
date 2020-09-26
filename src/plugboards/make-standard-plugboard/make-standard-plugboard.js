1bb6a154d348d14711f36ba45fe95f06b5726f57
Peter Lawless <peterlawless223@gmail.com>
2020-09-25 20:00:00 -0400

    import validators from utils

--- src/plugboards/make-standard-plugboard/make-standard-plugboard.js
+++ src/plugboards/make-standard-plugboard/make-standard-plugboard.js
@@ -1,20 +1,9 @@
 import BiMap from "mnemonist/bi-map";
-import { getLetterMappingFrom, validateIsSingleLetter } from "../../utils";
-
-const validateUniqueMapping = (letter, biMap) => {
-  if (biMap.has(letter)) {
-    throw new Error(`dual mapping for letter: ${letter}`);
-  }
-};
+import { getLetterMappingFrom, validateUniqueMapping } from "../../utils";
 
 const makeStandardPlugboard = letterPairs => {
   const plugboard = BiMap.from(letterPairs);
-  for (let [key, value] of plugboard) {
-    validateIsSingleLetter(key);
-    validateIsSingleLetter(value);
-    validateUniqueMapping(key, plugboard.inverse);
-    validateUniqueMapping(value, plugboard);
-  }
+  validateUniqueMapping(plugboard);
   return getLetterMappingFrom(plugboard);
 };
 
